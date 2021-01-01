2faa5761a12bbebc44e77104a3632263e35e66ba
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-01 17:54:26 +0700

    6. ENV and Toast Notifications

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
 import Header from "./components/nav/Header";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
 
+
 function App() {
   return (
     <>
       <Header />
+      <ToastContainer />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />

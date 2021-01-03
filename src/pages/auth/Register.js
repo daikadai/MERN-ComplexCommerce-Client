c093b8770885ee4868a5f92d99b6b011c54491a5
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-03 16:03:48 +0700

    17. Redirect logged in user

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify"
-
-const Register = () => {
+import { useSelector } from "react-redux";
+const Register = ({history}) => {
   const [email, setEmail] = useState("");
+  
+  const { user } = useSelector(state => state)
+
+  useEffect(() => {
+    if(user && user.token) {
+      history.push('/')
+    }
+  }, [user])
 
   const handleSubmit = async (e) => {
     e.preventDefault()

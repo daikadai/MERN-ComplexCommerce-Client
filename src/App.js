7fbdfa12784023be73b753148f8391bb08c7bc53
Pham Cao Dai <daikadai1026@gmail.com>
2021-01-03 15:55:02 +0700

    16. Forgot Password

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import RegisterComplete from "./pages/auth/RegisterComplete";
 import Home from "./pages/Home";
 import { auth } from "./firebase";
 import { useDispatch } from 'react-redux'
+import ForgotPassword from './pages/auth/ForgotPassword';
 
 
 function App() {
@@ -43,6 +44,7 @@ function App() {
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <Route exact path="/register/complete" component={RegisterComplete} />
+        <Route exact path="/forgot/password" component={ForgotPassword} />
       </Switch>
     </>
   );
